d4364629d274d0dad23e9905846fd99d894835de
YubeRodri <yrodriguez@uamv.edu.ni>
2025-10-23 18:35:37 -0600

    Agregad de carrera

--- src/main/java/ni/edu/uam/uam_app/modelo/Carrera.java
+++ src/main/java/ni/edu/uam/uam_app/modelo/Carrera.java
@@ -1,4 +1,24 @@
 package ni.edu.uam.uam_app.modelo;
 
+
+import lombok.Getter;
+import lombok.Setter;
+import org.openxava.annotations.Required;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
 public class Carrera {
+        @Id
+        private String Id;
+        @Column(name = "Nombre", length = 30, nullable = false)
+        @Required(message = "Nombre no puede quedar vacio...")
+        private String nombre;
+        private String descripcion;
+
 }
